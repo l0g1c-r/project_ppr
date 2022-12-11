a498b2804ff376d05584049f2acf22963b32aa39
logic <raliphadalogic@Gmail.com>
2022-12-11 14:21:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const pic1 = document.getElementById("p1");
+pic1.onclick = () => alert("Stellenbosch Shenanigans!!");
 
-pic1.onclick = () => alert("Stellenbosch shenanigans!!!");
+const pic2 = document.getElementById("p2");
+pic2.onclick = () => alert("Baby me being mad!! 💜");
 
-// window.onresize = () => {
-//     pxlSize.textContent = window.innerWidth;
-// }
+const pic3 = document.getElementById("p3");
+pic3.onclick = () => alert("Cute me!! 😊");
+
+const pic4 = document.getElementById("lngImg");
+pic4.onclick = () => alert("Birthday Vibes!! 🎂🥳🎉");
